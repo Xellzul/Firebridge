9de7cb482b448774623a162751b401ee9a0dfb0d
David Podzimek <davidpodzimek@windowslive.com>
2019-08-21 16:22:09 +0200

    [Added models]

---
+++ FirebridgeClient/Models/ZombieProgramModel.cs
@@ -0,0 +1,16 @@
+﻿using FirebridgeShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebridgeClient.Models
+{
+    class ZombieProgramModel
+    {
+        public MimiProgramModel Program { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FirebridgeShared/Models/MiniProgramsModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebridgeShared.Models
+{
+
+    public class MimiProgramModel
+    {
+        public List<string> References { get; set; }
+        public string Code { get; set; }
+    }
+}
