63e73192788156d0228a9a8d474dcf84f35ab870
David Podzimek <davidpodzimek@windowslive.com>
2019-08-21 16:01:20 +0200

    [UI update]

--- FirebridgeClient/Form1.Designer.cs
+++ FirebridgeClient/Form1.Designer.cs
@@ -30,26 +30,133 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this._buttonConsoleSubmit = new System.Windows.Forms.Button();
+            this._textBoxSubmitText = new System.Windows.Forms.TextBox();
+            this._textBoxConsole = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 422);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 396);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this._buttonConsoleSubmit);
+            this.tabPage2.Controls.Add(this._textBoxSubmitText);
+            this.tabPage2.Controls.Add(this._textBoxConsole);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 396);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Console";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // _buttonConsoleSubmit
+            // 
+            this._buttonConsoleSubmit.Location = new System.Drawing.Point(709, 366);
+            this._buttonConsoleSubmit.Name = "_buttonConsoleSubmit";
+            this._buttonConsoleSubmit.Size = new System.Drawing.Size(75, 23);
+            this._buttonConsoleSubmit.TabIndex = 2;
+            this._buttonConsoleSubmit.Text = "Submit";
+            this._buttonConsoleSubmit.UseVisualStyleBackColor = true;
+            this._buttonConsoleSubmit.Click += new System.EventHandler(this._buttonConsoleSubmit_Click);
+            // 
+            // _textBoxSubmitText
+            // 
+            this._textBoxSubmitText.Location = new System.Drawing.Point(8, 369);
+            this._textBoxSubmitText.Name = "_textBoxSubmitText";
+            this._textBoxSubmitText.Size = new System.Drawing.Size(695, 20);
+            this._textBoxSubmitText.TabIndex = 1;
+            // 
+            // _textBoxConsole
+            // 
+            this._textBoxConsole.BackColor = System.Drawing.SystemColors.HighlightText;
+            this._textBoxConsole.Enabled = false;
+            this._textBoxConsole.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this._textBoxConsole.Location = new System.Drawing.Point(6, 6);
+            this._textBoxConsole.Name = "_textBoxConsole";
+            this._textBoxConsole.ReadOnly = true;
+            this._textBoxConsole.Size = new System.Drawing.Size(778, 355);
+            this._textBoxConsole.TabIndex = 0;
+            this._textBoxConsole.Text = "";
+            this._textBoxConsole.TextChanged += new System.EventHandler(this._textBoxConsole_TextChanged);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(792, 396);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button _buttonConsoleSubmit;
+        private System.Windows.Forms.TextBox _textBoxSubmitText;
+        private System.Windows.Forms.RichTextBox _textBoxConsole;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
