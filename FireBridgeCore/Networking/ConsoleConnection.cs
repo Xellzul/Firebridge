c59662c039a6db7db87dfca39015ec52b6d8d7e9
Hlavaty Adrian <xellzul@gmail.com>
2021-02-08 17:26:11 +0100

    Console Conenction class

---
+++ FireBridgeCore/Networking/ConsoleConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FireBridgeCore.Networking
+{
+    public class ConsoleConnection : Connection
+    {
+        public virtual void Start(Stream reader, Stream writer)
+        {
+            if (_shouldEnd)
+                return;
+
+            Status = ConnectionStatus.Connecting;
+
+            _readStream = reader;
+            _writeStream = writer;
+
+            this._readerThread = new Thread(ReadLoop);
+            this._readerThread.Start();
+
+            Status = ConnectionStatus.Connected;
+        }
+
+        public override void Close()
+        {
+            Status = ConnectionStatus.Disconnected;
+            _shouldEnd = true;
+            _readStream = null;
+            _writeStream = null;
+        }
+
+    }
+}
